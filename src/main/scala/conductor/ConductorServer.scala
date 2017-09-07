84e0afa21932cfe5fa4e13e096e9f28f1ffb1fdd
Colin Slater <ctslater@uw.edu>
2017-09-07 14:28:14 -0700

    Reorganization, test HTTP calls.

--- src/main/scala/conductor/ConductorServer.scala
+++ src/main/scala/conductor/ConductorServer.scala
@@ -11,32 +11,56 @@ import scala.io.StdIn
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 
+
+trait ConductorService {
+
+  implicit val system:ActorSystem
+  implicit val materializer:ActorMaterializer
+  // needed for the future flatMap/onComplete in the end
+  val executionContext = system.dispatcher
+
+  implicit val priorityTasksFormat = jsonFormat1(PriorityPartitions)
+
+  val conductor = new Conductor
+  conductor.refreshPartitionsForJob(JobId(0),
+                                    List("partition1", "partition2"))
+  conductor.refreshPartitionsForJob(JobId(1),
+                                    List("partition1", "partition3"))
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    } ~
+    path("getPartitions") {
+      get {
+        complete(conductor.getPriorityPartitions)
+      }
+    } /* ~
+    path("updatePartitions") {
+      put {
+        decodeRequest {
+          entity(as[JobUpdate]) {  update => conductor.refreshPartitionsForJob(update) }
+        }
+      }
+    } */
+
+}
+
+class ConductorServer(implicit val system:ActorSystem,
+  implicit val materializer:ActorMaterializer) extends ConductorService
+
 object ConductorServer {
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    implicit val priorityTasksFormat = jsonFormat1(PriorityPartitions)
+    val server = new ConductorServer
+    val bindingFuture = Http().bindAndHandle(server.route, "localhost", 8080)
 
-    val tasks = PriorityPartitions(List("blah", "asdf"))
-
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      } ~
-      path("test") {
-        get {
-          complete(tasks)
-        }
-
-      }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
--- src/test/scala/conductor/ConductorTest.scala
+++ src/test/scala/conductor/ConductorTest.scala
@@ -3,6 +3,13 @@
 package conductor
 
 import org.scalatest.{FlatSpec, OptionValues, Matchers}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.util.ByteString
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server._
+import Directives._
+
+
 // import conductor.Conductor
 
 abstract class UnitSpec extends FlatSpec with OptionValues with Matchers
@@ -13,7 +20,7 @@ class ConductorTest extends UnitSpec {
     conductor.state.partitionsByJobId.size shouldBe 0
   }
 
-  "Conductor" should "Update state with new partitions" in {
+  "Conductor" should "update state with new partitions" in {
     // This internal reads state and should be removed.
     val job = JobId(0)
     val partitions = List("partition1", "partition2")
@@ -23,7 +30,7 @@ class ConductorTest extends UnitSpec {
     partitionsForJob.size shouldBe 1
   }
 
-  "Conductor" should "Return partitions after update" in {
+  "Conductor" should "return partitions after update" in {
     // This internal reads state and should be removed.
     val job = JobId(0)
     val partitions = List("partition1", "partition2")
@@ -32,10 +39,8 @@ class ConductorTest extends UnitSpec {
     conductor.getPriorityPartitions().files.size shouldBe 0
   }
 
-  "Conductor" should "Return most common partition" in {
+  "Conductor" should "return most common partition" in {
     // This internal reads state and should be removed.
-    val job = JobId(0)
-    val partitions = List("partition1", "partition2")
     val conductor = new Conductor
     conductor.refreshPartitionsForJob(JobId(0),
                                       List("partition1", "partition2"))
@@ -46,4 +51,39 @@ class ConductorTest extends UnitSpec {
     conductor.getPriorityPartitions().files should not contain ("partition3")
   }
 
+  "Conductor" should "accept job updates" in {
+    // This internal reads state and should be removed.
+    val conductor = new Conductor
+    conductor.refreshPartitionsForJob(JobId(0),
+                                      List("partition1", "partition2"))
+    conductor.refreshPartitionsForJob(JobId(1),
+                                      List("partition1", "partition2"))
+    conductor.refreshPartitionsForJob(JobId(0), List("partition2"))
+    conductor.getPriorityPartitions().files shouldBe List("partition2")
+    }
+
+}
+
+class ConductorHttpTest extends UnitSpec with ScalatestRouteTest with ConductorService {
+
+  "ConductorApi" should "Accept job updates" in {
+
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "job_id": "7",
+           |    "partitions": {"part1","part2","part3"}
+           |}
+        """.stripMargin)
+
+      val postRequest = HttpRequest(
+        HttpMethods.POST,
+        uri = "/partitions",
+        entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+      postRequest ~> route ~> check {
+           status.isSuccess() shouldEqual true
+    }
+
+  }
 }
